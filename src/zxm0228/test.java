a68d5d643a1d7e76e89e530604911872127106f0
m2666666 <1051738324@qq.com>
2019-04-07 19:17:37 +0800

    第一次

---
+++ src/zxm0228/test.java
@@ -0,0 +1,12 @@
+package zxm0228;
+
+public class test {
+public static void main(String[] args) {
+
+   int b=10;
+
+   StringBuffer a = new StringBuffer("");  
+
+	/*System.out.println("a="+a1+",b="+b+",sum="+sum);*/
+}
+}
