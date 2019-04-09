25093bbbdb97b92efc01ded5c13639bd7770bfff
m2666666 <1051738324@qq.com>
2019-04-09 09:28:16 +0800

    恢复

---
+++ src/zxm0228/test.java
@@ -0,0 +1,12 @@
+package zxm0228;
+
+public class test {
+public static void main(String[] args) {
+
+   int b=111;
+
+   StringBuffer a = new StringBuffer("");  
+
+	/*System.out.println("a="+a1+",b="+b+",sum="+sum);*/
+}
+}
